c27edf5662851aa06078c0b3203ed2d86de311a3
Sotatek-HaiTrieu2 <hai.trieu2@sotatek.com>
2023-08-30 16:22:04 +0700

    fix: update script

--- scripts/start_bandd.sh
+++ scripts/start_bandd.sh
@@ -1,2 +1,7 @@
 # start bandchain
-bandd start --rpc.laddr tcp://0.0.0.0:26657
+bandd start \
+--p2p.laddr tcp://0.0.0.0:36656 \
+--rpc.laddr tcp://0.0.0.0:36657 \
+--grpc-web.address 0.0.0.0:10091 \
+--grpc.address 0.0.0.0:10090 \
+--rpc.pprof_laddr 0.0.0.0:7060 \
--- scripts/start_yoda.sh
+++ scripts/start_yoda.sh
@@ -5,11 +5,14 @@ rm -rf ~/.yoda
 # config chain id
 yoda config chain-id bandchain
 
+yoda config node tcp://localhost:36657
+
 # add validator to yoda config
 yoda config validator $(bandd keys show validator -a --bech val --keyring-backend test)
 
 # setup execution endpoint
-yoda config executor "rest:$EXECUTOR_URL/live/master?timeout=10s"
+# yoda config executor "rest:$EXECUTOR_URL/live/master?timeout=10s"
+yoda config executor "rest:http://localhost:5000?timeout=10s"
 
 # setup broadcast-timeout to yoda config
 yoda config broadcast-timeout "5m"
@@ -20,8 +23,7 @@ yoda config rpc-poll-interval "1s"
 # setup max-try to yoda config
 yoda config max-try 5
 
-echo "y" | bandd tx oracle activate --from validator --keyring-backend test --chain-id bandchain
-
+echo "y" | bandd tx oracle activate --from validator --keyring-backend test --chain-id bandchain --node tcp://localhost:36657
 # wait for activation transaction success
 sleep 2
 
@@ -32,13 +34,13 @@ do
 done
 
 # send band tokens to reporters
-echo "y" | bandd tx bank send  validator $(yoda keys list -a) 1000000uband --keyring-backend test --chain-id bandchain
+echo "y" | bandd tx bank send  validator $(yoda keys list -a) 1000000uband --keyring-backend test --chain-id bandchain --node tcp://localhost:36657
 
 # wait for sending band tokens transaction success
 sleep 2
 
 # add reporter to bandchain
-echo "y" | bandd tx oracle add-reporters $(yoda keys list -a) --from validator --keyring-backend test --chain-id bandchain
+echo "y" | bandd tx oracle add-reporters $(yoda keys list -a) --from validator --keyring-backend test --chain-id bandchain --node tcp://localhost:36657
 
 # wait for addding reporter transaction success
 sleep 2
